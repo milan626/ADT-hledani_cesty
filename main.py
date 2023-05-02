4b6725ef7e39918f48f4ba2caa157b69bb005220
Milan Marek <milda.marek@email.cz>
2023-05-02 21:35:07 +0200

    Načtený graf, vrcholy jsou objekty nodů, jejich váha je vzádlenost v metrech

--- main.py
+++ main.py
@@ -3,14 +3,34 @@
 # https://github.com/JakubSido/adthelpers
 # pip install git+
 
-import geopy.distance
+from shapely.wkt import loads
+import geopy.distance as geoD
 import adthelpers
 import networkx as nx
 from queue import PriorityQueue
 import numpy as np
 import json
 import random
+
+from tqdm import tqdm
 np.random.seed(19680801)
+
+
+class Node:
+    def __init__(self, linestring) -> None:
+        self.linestring = linestring
+
+    def __repr__(self) -> str:
+        return str(self.get_cordinates())
+    
+    def __str__(self) -> str:
+        return str(self.get_cordinates())
+
+    def get_cordinates(self):
+        temp  = self.linestring.replace('POINT(', "")
+        temp = temp.replace(')', "")
+        y, x = temp.split(" ")
+        return (float(x), float(y))
 
 def generate_graph():
     graph = nx.grid_2d_graph(3, 3)
@@ -56,16 +76,16 @@ def main():
     while(q.qsize() > 0):
 
         current_distance, active_node = q.get()
-        print(graph.edges(active_node))
+        # print(graph.edges(active_node))
         if active_node in closed: # this-ucitel
-            print("Konec prochazeni grafu")
+            # print("Konec prochazeni grafu")
             continue
 
         edges = graph.edges(active_node)
 
         for e in edges:
             soused = e[1]
-            print(soused, distances[soused])
+            # print(soused, distances[soused])
             weight = graph.get_edge_data(active_node, soused)['weight'] # vaha cesty od aktualniho nodu k sousedovi
 
             new_distance = current_distance + weight
@@ -87,10 +107,10 @@ def main():
 
         closed.add(active_node)
     
-    print(distances)
-    print(predecessors)
-    get_path((2,2), predecessors)
-    painter.draw_graph(active_node)
+    # print(distances)
+    # print(predecessors)
+    # get_path((2,2), predecessors)
+    # painter.draw_graph(active_node)
 
 def get_path(cil, predecessors):
     path = predecessors[cil]
@@ -102,8 +122,25 @@ def get_path(cil, predecessors):
 
     print("Celkova cesta: ",cesta)
         
-def load_graph():
-    pass
+def load_graph(edges_path, nodes_path):
+    G = nx.DiGraph()
+    nodes = load_nodes(nodes_path)
+
+    with open(edges_path, "r+") as file:
+        file.readline()
+        lines = file.readlines()
+
+        for line in tqdm(lines):
+            if line.strip() == "":
+                continue
+
+            node_id,source,target,capacity,isvalid,WKT = line.split(",", 5)
+            G.add_edge(int(source), int(target), weight=get_distance(WKT))
+
+    return G, nodes
+
+
+
 # G = nx.DiGraph()
 # G.add_edge(source, dest, weight = 4)
 
@@ -113,15 +150,82 @@ def load_graph():
 # v edges je automatickz jednosmerna cesta
 # sirka a delka jsou prohozeni v souborech
 
+def load_nodes(filepath):
+    nodes = dict()
+    with open(filepath, "r+") as file:
+        file.readline()
+        lines = file.readlines()
+        for line in lines:
+            node_id, linestring = line.split(",")
+            linestring = linestring.replace('\"', "").strip()
+            nodes[int(node_id)] = Node(linestring)
+
+    return nodes
+
+
+
+def get_distance(long_ls):
+    # a number of other elipsoids are supported
+    distance = 0
+    geoD.geodesic.ELLIPSOID = 'WGS-84'
+    d = geoD.distance
+
+    list_of_ls = split_LineString(long_ls)
+
+    for ls in list_of_ls:
+        line = loads(ls)
+        # convert the coordinates to xy array elements, compute the distance
+        dist = d(line.xy[0], line.xy[1])
+        # print(dist.meters)
+        distance += dist.meters
+
+    return distance
+
+def split_LineString(original):
+    result = list()
+    prefix = "LINESTRING("
+    postfix = ")"
+    original = original.replace(prefix, "").strip()
+    original = original.replace(postfix, "")
+    original = original.replace('\"', '')
+    splitted = original.split(",")
+
+    for i in range(0, len(splitted)-1):
+        long1, lat1 = splitted[i].split(" ")
+        long2, lat2 = splitted[i+1].split(" ")
+        newLS = prefix + lat1 + " " + lat2 + "," + long1 + " " + long2 + postfix
+        # newLS = prefix + lat1 + " " + long1 + "," + lat2 + " " + long2 + postfix
+        # print(newLS)
+        result.append(newLS)
+
+    return result
+
 
 if __name__ == '__main__':
+    
 
+    linestring = "LINESTRING(13.2493302001435 49.764380533239,13.249074682493 49.7658087519211,13.2488225160078 49.7674230526761,13.2485899684292 49.7692554591859,13.248584957672 49.7692712940434,13.2483006280357 49.7698227628131,13.2480651174741 49.7705679279674,13.2479923746268 49.7715163332597,13.2482168296308 49.7721304869104,13.2486411097935 49.7726666339142,13.2486415190555 49.7726671566612,13.2494821842308 49.7737525730256,13.2500302202536 49.7741557317799)"
+    # print(get_distance(linestring))
 
-    coords_1 = (52.2296756, 21.0122287)
-    coords_2 = (52.406374, 16.9251681)
+    graph, nodes = load_graph("data/pilsen_edges.csv", "data/pilsen_nodes.csv")
 
-    print(geopy.distance.geodesic(coords_1, coords_2).km)
+    print("Data loaded")
+    while(True):
+        inp = input()
+        edges = graph.edges(int(inp))
+        print(edges)
 
-    main()
-    # hledani minimalni kostry grafu
-    # todo - networkX knihovna
+        print("Nodes:")
+        for start,end in edges:
+            print("-------")
+            print(nodes[int(start)].linestring)
+            print(nodes[int(end)].linestring)
+            print(graph.get_edge_data(start, end))
+            
+    
+    # main()
+
+    """
+        Nyní bych měl mít správně načtený graf snad, nutno ještě ověřit
+        Nody v grafu jsou identifikovatelný dle IDček
+    """
