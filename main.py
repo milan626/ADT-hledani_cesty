995a09d2baf8da0282f03fa9ca0286f0cf7600cf
Milan Marek <milda.marek@email.cz>
2023-05-03 21:17:25 +0200

    Konečný refactor, odstraněný  zakomentovaného kódu, vylepšení logiky některých metod

--- main.py
+++ main.py
@@ -54,9 +54,7 @@ def load_graphs(filepath):
 def print_queue(q : PriorityQueue):
     pass
     
-def dijkstra(start_node, end_node, graph):
-    
-    # spanning_tree = set()
+def dijkstra(start_node, graph):
     closed = set()
     q = PriorityQueue()
 
@@ -72,45 +70,28 @@ def dijkstra(start_node, end_node, graph):
 
     distances[start_node] = 0
 
-    # painter = adthelpers.painter.Painter(graph, q, closed, None, distances= distances)
-
     while(q.qsize() > 0):
 
         current_distance, active_node = q.get()
-        # print(graph.edges(active_node))
-        if active_node in closed: # this-ucitel
-            # print("Konec prochazeni grafu")
+        if active_node in closed:
             continue
 
         edges = graph.edges(active_node)
 
         for e in edges:
             soused = e[1]
-            # print(soused, distances[soused])
             weight = graph.get_edge_data(active_node, soused)['weight'] # vaha cesty od aktualniho nodu k sousedovi
 
             new_distance = current_distance + weight
 
             if(new_distance < distances[soused]):
-                # print(current_distance, "+", weight , "<" , distances[soused])
                 distances[soused] = new_distance
                 predecessors[soused] = active_node
-                # print(soused)
                 q.put((new_distance, soused))
-                # print(distances[soused])
-        # painter.draw_graph(active_node)
 
-
-            # sousedi_souseda = graph.edges(e[1])
-            # print(sousedi_souseda)
-            # fr, to = e[0], e[1] # To jsou dva body
-            # w = graph.get_edge_data(fr, to)['weight']
-            # q.put((w, to, e))
 
         closed.add(active_node)
-    
-    # print(distances)
-    # print(predecessors)
+
     return distances, predecessors
 
 def get_path(cil, predecessors):
@@ -121,7 +102,7 @@ def get_path(cil, predecessors):
         cesta.append(path)
         path = predecessors[path]
 
-    print("Celkova cesta: ",cesta)
+    # print("Celkova cesta: ",cesta)
     return cesta
         
 def load_graph(edges_path, nodes_path):
@@ -142,16 +123,6 @@ def load_graph(edges_path, nodes_path):
     return G, nodes
 
 
-
-# G = nx.DiGraph()
-# G.add_edge(source, dest, weight = 4)
-
-# aproxiamce kouli,  vzdalenost dopocitat z line stringu, knihovna 
-# 4651 hradek
-# 4559 zbuch - vypocitat jejich cestu
-# v edges je automatickz jednosmerna cesta
-# sirka a delka jsou prohozeni v souborech
-
 def load_nodes(filepath):
     nodes = dict()
     with open(filepath, "r+") as file:
@@ -167,7 +138,6 @@ def load_nodes(filepath):
 
 
 def get_distance(long_ls):
-    # a number of other elipsoids are supported
     distance = 0
     geoD.geodesic.ELLIPSOID = 'WGS-84'
     d = geoD.distance
@@ -176,9 +146,7 @@ def get_distance(long_ls):
 
     for ls in list_of_ls:
         line = loads(ls)
-        # convert the coordinates to xy array elements, compute the distance
         dist = d(line.xy[0], line.xy[1])
-        # print(dist.meters)
         distance += dist.meters
 
     return distance
@@ -216,8 +184,6 @@ def split_LineString(original):
         long1, lat1 = splitted[i].split(" ")
         long2, lat2 = splitted[i+1].split(" ")
         newLS = prefix + lat1 + " " + lat2 + "," + long1 + " " + long2 + postfix
-        # newLS = prefix + lat1 + " " + long1 + "," + lat2 + " " + long2 + postfix
-        # print(newLS)
         result.append(newLS)
 
     return result
@@ -229,10 +195,8 @@ def get_final_distance(path, nodes, graph, plot):
     with open("output.txt", "w") as file:
         with open("output_edges.txt", "w") as file_e:
             for p in range(len(path)-1):
-                # print(path[i])
                 to_node_id = int(path[i])
                 i += 1
-                # print(path[i])
                 from_node_id = int(path[i])
                 edge_data = graph.get_edge_data(from_node_id, to_node_id)
                 length = edge_data['weight']
@@ -243,58 +207,53 @@ def get_final_distance(path, nodes, graph, plot):
                 file_e.write(WKT + ",")
 
                 text = nodes[to_node_id].linestring + ",\n"
-                # text = nodes[from_node_id].linestring + ", "+ nodes[to_node_id].linestring + ",\n"
                 file.write(text)
 
                 final_distance += length
 
             file.write(nodes[path[-1]].linestring)
 
-
     plot.save()
+    return final_distance
 
-    print("Celkova vzdalenost: " + str(final_distance))
-
-
-    # linestring = "LINESTRING(13.2493302001435 49.764380533239,13.249074682493 49.7658087519211,13.2488225160078 49.7674230526761,13.2485899684292 49.7692554591859,13.248584957672 49.7692712940434,13.2483006280357 49.7698227628131,13.2480651174741 49.7705679279674,13.2479923746268 49.7715163332597,13.2482168296308 49.7721304869104,13.2486411097935 49.7726666339142,13.2486415190555 49.7726671566612,13.2494821842308 49.7737525730256,13.2500302202536 49.7741557317799)"
-    # print(get_distance(linestring))
 def main():
     graph, nodes = load_graph("data/pilsen_edges.csv", "data/pilsen_nodes.csv")
     start_node = 4569
     end_node = 4651
 
-    distances, predecessors = dijkstra(start_node, end_node, graph)
+    distances, predecessors = dijkstra(start_node, graph)
+
     path = get_path(end_node, predecessors)
     
-    plot = OpenStreetMapsPlot("Open Street Map Plot",height=1000,width=1000,)
-    get_final_distance(path, nodes, graph, plot)
+    plot = OpenStreetMapsPlot("Open Street Map Plot",height=1000,width=1200)
+
+    # Zároveň vytvoří 3 soubory, 
+    final_dist = get_final_distance(path, nodes, graph, plot)
+    print("Celkova vzdalenost: " + str(final_dist) + "km")
     plot.show()
+
+
     while(True):
+        # aby se nezavrel prohlizec
         pass
+    
 
     
 
 if __name__ == '__main__':
     main()
 
-    # print("Data loaded")
-    # while(True):
-    #     inp = input()
-    #     edges = graph.edges(int(inp))
-    #     print(edges)
-
-    #     print("Nodes:")
-    #     for start,end in edges:
-    #         print("-------")
-    #         print(nodes[int(start)].linestring)
-    #         print(nodes[int(end)].linestring)
-    #         print(graph.get_edge_data(start, end))
-    # print("(////////////////////////////")
-
-    # graph = generate_graph()
-    # main((0,0), (2,2), graph)
-
     """
     test - hledani nejkratsi cesty a minimalni kostry
 
     """
+
+    
+# G = nx.DiGraph()
+# G.add_edge(source, dest, weight = 4)
+
+# aproxiamce kouli,  vzdalenost dopocitat z line stringu, knihovna 
+# 4651 hradek
+# 4559 zbuch - vypocitat jejich cestu
+# v edges je automatickz jednosmerna cesta
+# sirka a delka jsou prohozeni v souborech
